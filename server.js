951bb2f9d4a028ba6845fbae693ac015e2d52043
Takaedza <tmandie@live.com>
2025-03-07 11:08:21 +0200

    codec

--- server.js
+++ server.js
@@ -9,11 +9,24 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const expressEjsLayouts = require("express-ejs-layouts")
+
 
 /* ***********************
  * Routes
  *************************/
+app.set("view engine", "ejs")
+app.use(require("express-ejs-layouts"))
+app.set("layout", "./layouts/layout") // not at views root
+/* ***********************
+ * Routes
+ *************************/
 app.use(static)
+
+// Index route
+app.get("/", function (req, res) {
+  res.render("index", { title: "Home" })
+})
 
 /* ***********************
  * Local Server Information
